6ba5b810cda0267d03daa6c29cc37520a3e51274
Danial Emad <danialemad62@gmail.com>
2026-02-02 17:42:06 +0200

    Update InventoryPage.java

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -1,4 +1,33 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class InventoryPage {
+
+    private WebDriver driver;
+
+    private By pageTitle = By.cssSelector(".app_logo");
+    private By cartIcon = By.id("shopping_cart_container");
+    private By products = By.className("inventory_item");
+
+    public InventoryPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getPageTitle() {
+        return driver.findElement(pageTitle).getText();
+    }
+
+    public boolean isCartIconDisplayed() {
+        return driver.findElement(cartIcon).isDisplayed();
+    }
+
+    public int getProductsCount() {
+        List<WebElement> productList = driver.findElements(products);
+        return productList.size();
+    }
 }
