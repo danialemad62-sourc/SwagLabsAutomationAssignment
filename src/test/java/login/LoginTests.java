03ea18f66379c35349d0236de6d4666e0ff93730
Danial Emad <danialemad62@gmail.com>
2026-02-02 17:44:09 +0200

    Update LoginTests.java

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -1,4 +1,43 @@
 package login;
 
-public class LoginTests {
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+public class LoginTests extends BaseTest {
+
+    @Test
+    public void verifyValidLogin() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+
+        Assert.assertTrue(
+                driver.getCurrentUrl().contains("inventory.html"),
+                "Valid login failed"
+        );
+    }
+
+    @Test
+    public void verifyInvalidLogin() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("wrong_user", "wrong_pass");
+
+        String error = loginPage.getErrorMessage();
+        Assert.assertTrue(
+                error.contains("Username and password do not match"),
+                "Error message not correct for invalid login"
+        );
+    }
+
+    @Test
+    public void verifyLoginWithoutPassword() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "");
+
+        String error = loginPage.getErrorMessage();
+        Assert.assertTrue(
+                error.contains("Password is required"),
+                "Error message not correct for empty password"
+        );
+    }
 }
